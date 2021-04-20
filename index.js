525c0f2453aecc0a61c253558eaf7aec6fd0b2cb
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-19 21:40:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
   // 2. A MainContent component
   // 3. A Footer component
 
+  import React from "react";
+  import ReactDOM from "react-dom";
+  
+  function MyInfo() {
+    return (
+      <div>
+        <h1>Juan Cruz</h1>
+        <p>This is practice to learn react</p>
+  
+        <ul>
+          <li>Dominican Republic</li>
+          <li>France</li>
+          <li>Spain</li>
+        </ul>
+      </div>
+    );
+  }
+  ReactDom.render(<MyInfo />, document.getElementById("root"));
