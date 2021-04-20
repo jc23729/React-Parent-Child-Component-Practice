990033d6d470e3d1ed0c0a387495beb078fe3cc0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-19 21:59:11 -0400

    update

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>this is a footer</h1>
+    </footer>
+  );
+}
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 // Set up the React app from scratch
 // Render an App component (defined in a separate file)
 // Inside App, render:
-  // 1. A Navbar component
-  // 2. A MainContent component
-  // 3. A Footer component
+// 1. A Navbar component
+// 2. A MainContent component
+// 3. A Footer component
 
-  import React from "react";
-  import ReactDOM from "react-dom";
-  
-  function MyInfo() {
-    return (
-      <div>
-        <h1>Juan Cruz</h1>
-        <p>This is practice to learn react</p>
-  
-        <ul>
-          <li>Dominican Republic</li>
-          <li>France</li>
-          <li>Spain</li>
-        </ul>
-      </div>
-    );
-  }
-  ReactDom.render(<MyInfo />, document.getElementById("root"));
+import React from "react";
+import Footer from "./Footer.js";
+
+function App() {
+  return (
+    <Footer/>
+  );
+}
+export default App;
