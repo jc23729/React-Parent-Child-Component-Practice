413a494589a034b439d991be4ad2da35fed7397d
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-19 22:06:35 -0400

    Update

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from "react"
+import React from "react";
 
-import Header from "./components/Header"
-import Header from "./components/MainContent"
-import Header from "./components/Footer"
+import Header from "./components/Header";
+import Header from "./components/MainContent";
+import Header from "./components/Footer";
 
 function App() {
-    return (
-       <div>
-           <Header/>
-           <MainContent/>
-           <Footer />
-       
-       
-       </div>
-    )
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
